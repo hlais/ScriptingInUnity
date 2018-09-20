73b404af41d98bbcb42412129be3c41d97ddff44
Halim Lais <halimlais@hotmail.com>
2018-09-20 20:02:42 +0100

    Initial commit

---
+++ Assets/Scripts/ChangingColor.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+
+
+public class ChangingColor : MonoBehaviour
+{
+    Renderer m_renderer;
+
+    public void Start()
+    {
+        m_renderer = GetComponent<Renderer>();
+    }
+
+    public void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            m_renderer.material.color = Color.red;
+        }
+        if (Input.GetKeyDown(KeyCode.G))
+        {
+           m_renderer.material.color = Color.green;
+        }
+        if (Input.GetKeyDown(KeyCode.B))
+        {
+            m_renderer.material.color = Color.blue;
+        }
+
+    }
+    private void OnMouseEnter()
+    {
+        m_renderer.material.color = Color.clear;
+    }
+    private void OnMouseOver()
+    {
+        m_renderer.material.color = Color.black;
+    }
+    private void OnMouseExit()
+    {
+        m_renderer.material.color = Color.cyan;
+    }
+}
---
+++ Assets/Scripts/ClockAnimator.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System;
+
+
+public class ClockAnimator : MonoBehaviour
+{
+    [SerializeField] Transform hours, mins, seconds;
+
+    private const float hoursToDegree = 360 / 12f;
+    private const float minsToDegree = 360 / 60f;
+    private const float secondsToDegree = 360 / 60f;
+
+    public bool analog;
+
+    private void Update()
+    {
+        if (analog)
+        {
+            TimeSpan time = DateTime.Now.TimeOfDay;
+            hours.localRotation = Quaternion.Euler(0, 0, hoursToDegree * -(float)time.TotalHours);
+            mins.localRotation = Quaternion.Euler(0, 0, minsToDegree * -(float)time.TotalMinutes);
+            seconds.localRotation = Quaternion.Euler(0, 0, secondsToDegree * -(float)time.TotalSeconds);
+            
+        }
+        else
+        {
+            DateTime time = DateTime.Now;
+            // minus values becuase Unity uses left hand co-ordinate system
+            hours.localRotation = Quaternion.Euler(0, 0, hoursToDegree * -time.Hour);
+            mins.localRotation = Quaternion.Euler(0, 0, minsToDegree * -time.Minute);
+            seconds.localRotation = Quaternion.Euler(0, 0, secondsToDegree * -time.Second);
+        }
+    }
+
+
+}
